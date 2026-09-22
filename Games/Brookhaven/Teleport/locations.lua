525a4f112db7acde11e25dd8f3e3f38bbf9f4002
GemmandKilua2010 <specterxofc@outlook.com>
2026-09-22 13:43:02 -0300

    Create locations.lua

---
+++ Games/Brookhaven/Teleport/locations.lua
@@ -0,0 +1,32 @@
+local Locates = {
+  ["Locations"] = {
+    {"Praça", Vector3.new(7.12, 3.30, 34.65)},
+  	{"Banco", Vector3.new(2.44, 3.50, 255.11)},
+  	{"Piscina da praça", Vector3.new(13.74, 3.87, 133.32)},
+  	{"Escola", Vector3.new(-305.96, 3.60, 212.04)},
+  	{"Hospital", Vector3.new(-304.59, 3.48, 24.01)},
+  	{"Delegacia", Vector3.new(-118.83, 3.49, 7.80)},
+  	{"Hotel", Vector3.new(249.18, 3.60, 215.39)},
+  	{"Lanchonete", Vector3.new(166.70, 4.50, 35.89)},
+  	{"Shopping", Vector3.new(161.21, 3.60, -168.92)},
+  	{"Creche", Vector3.new(-126.75, 3.60, 131.53)},
+  	{"Apartamento 1", Vector3.new(-42.31, 3.50, -121.11)},
+  	{"Apartamento 2", Vector3.new(-36.51, 19.00, -127.52)},
+  	{"Apartamento VIP", Vector3.new(-34.78, 35.00, -134.67)},
+  	{"Fliperama Arcade", Vector3.new(-168.92, 3.42, -125.58)},
+  	{"Prefeitura", Vector3.new(-354.94, 7.40, -104.12)},
+  	{"Corpo de Bombeiros", Vector3.new(-447.41, 3.50, -144.22)},
+  	{"Fazenda", Vector3.new(-846.09, 3.00, -438.49)}
+  },
+  
+  ["Secret Locations"] = {
+  	{"Passagem Subterrânea", Vector3.new(-92.76, -10.70, 263.66)},
+  	{"Sala das Câmeras", Vector3.new(-117.42, -27.50, 235.92)},
+  	{"Passagem Aquática", Vector3.new(10.67, -9.98, 187.02)},
+  	{"Passagem do Banco", Vector3.new(21.56, 25.90, 248.61)},
+  	{"Passagem do Hospital", Vector3.new(-340.49, 16.58, 70.68)},
+  	{"Casa Abandonada", Vector3.new(1035.85, 5.50, 54.20)}
+  }
+}
+
+return Locates
