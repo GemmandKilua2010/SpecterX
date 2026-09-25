aaa7789d07a6a44abd156e3a029c8507d434008f
GemmandKilua2010 <specterxofc@outlook.com>
2026-09-24 22:57:42 -0300

    Update config.lua

--- Core/config.lua
+++ Core/config.lua
@@ -1,8 +1,24 @@
+local ImageID = 94800455817009
 local Config = {
-  Title = "SpecterX",
-  SubTitle = "By Specter",
+    Title = "SpecterX",
+    SubTitle = "By Specter",
 
-  Icon = 94800455817009
+    Icon = {
+        Image = ImageID,
+        Transparency = 0,
+        Corner = 5
+    },
+
+    DiscordInvite = {
+        Name = "Discord",
+        Desc = "Entre no nosso Discord e acompanhe todas as atualizações do SpecterX.",
+        
+        Logo = ImageID,
+        Invite = "Em Breve!",
+        
+        RPC = false
+    }
 }
 
+Config.DiscordInvite.Name = Config.Title
 return Config
